d47e92cf9cade8cd49b89ea1049d1ba1c68da0c2
asil <9745019+asiill@users.noreply.github.com>
2023-08-03 22:52:54 -0400

    Add script.js

---
+++ script.js
@@ -0,0 +1,21 @@
+const player = (sign) => {
+    this.sign = sign;
+
+    const getSign = () => {
+        return sign;
+    };
+
+    return {getSign};
+};
+
+const gameBoard = (() => {
+
+})();
+
+const gameController = (() => {
+
+})();
+
+const screenController = (() => {
+
+})();
