5cf836aa14b0d59f43110ae4c9d320d06055dc16
asil <9745019+asiill@users.noreply.github.com>
2023-08-05 10:44:14 -0400

    Write 'gameController' and 'screenController' function expressions

--- script.js
+++ script.js
@@ -29,9 +29,51 @@ const gameBoard = (() => {
 })();
 
 const gameController = (() => {
+    const playerX = player("X");
+    const playerY = player("Y");
+    let currentPlayer = playerX;
 
+    const switchCurrentPlayer = () => {
+        currentPlayer = (currentPlayer === playerX) ? playerY : playerX;
+    };
+
+    const playRound = (index) => {
+        gameBoard.setSign(index, currentPlayer.getSign());
+        switchCurrentPlayer();
+        screenController.setCurrentPlayer(currentPlayer);
+    };
+
+    return {playRound};
 })();
 
 const screenController = (() => {
+    const screenCells = document.querySelectorAll(".cell");
+    const gameResult = document.getElementById("game-result");
+    const currentPlayer = document.getElementById("current-player");
+    const resetBtn = document.getElementById("reset-btn");
 
+    currentPlayer.textContent = "Player X";
+
+    const updateScreen = () => {
+        for(i = 0; i < screenCells.length; i++) {
+            screenCells[i].textContent = gameBoard.getSign(i);
+        }
+    };
+
+    const setCurrentPlayer = (player) => {
+        currentPlayer.textContent = "Player " + player.getSign();
+    }
+
+    screenCells.forEach(cell => cell.addEventListener("click", (e) => {
+        if (e.target.textContent !== "") return;
+        gameController.playRound(parseInt(e.target.id));
+        updateScreen();
+    }));
+
+    resetBtn.addEventListener("click", () => {
+        gameBoard.resetBoard();
+        updateScreen();
+    });
+
+    return {setCurrentPlayer};
 })();
